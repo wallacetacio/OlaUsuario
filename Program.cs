c3393e72b8fe3bf6b0f9c5e44c7594f85b43d261
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-25 23:35:05 -0300

    Adiciona resolução do exercício OlaUsuario ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDigitado;
+            Console.Write("Por gentileza, digite seu nome: ");
+            nomeDigitado = Console.ReadLine();
+            Console.WriteLine($"Olá {nomeDigitado}!");
+        }
+    }
+}
